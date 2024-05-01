394534e0f77c0a47a2ef7dc4a6156576402921d6
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-05-01 19:07:54 -0300

    feat: Product Card component styles
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@ import { Header } from '@/components/header';
 import { Products } from '@/components/products';
 import { getProducts } from '@/util/getProducts';
 
+import '../scss/main.scss';
+
 export default async function Home() {
   const products = await getProducts();
   return (
     <>
       <Header />
-      <main>
-        <Products products={products} />
+      <main className="main-container">
+        <section>
+          <Products products={products} />
+        </section>
       </main>
       <Footer />
     </>
--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { getProducts } from '@/util/getProducts';
 import { useQuery } from '@tanstack/react-query';
+import '../scss/product-card-container.scss';
 
 interface ProductsProps {
   id: number;
@@ -25,18 +26,27 @@ export function Products({ products }: ProductsPropsArray) {
   if (error) return <div>Error fetching products</div>;
 
   return (
-    <div>
-      <h2>Products</h2>
-      <ul>
-        {data.products.map((product: ProductsProps) => (
-          <li key={product.id}>
+    <>
+      {data.products.map((product: ProductsProps) => (
+        <div key={product.id} className="product-card-container">
+          <div className="product-card-container-wrapper-img">
             <img src={product.photo} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <p>Price: ${product.price}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+          </div>
+
+          <div className="product-card-container-wrapper">
+            <h2>{product.name}</h2>
+            <span>R${product.price}</span>
+          </div>
+
+          <p className="product-card-container-wrapper-subtitle">
+            {product.description}
+          </p>
+
+          <a href="#" className="product-card-container-wrapper-link-buy">
+            Comprar
+          </a>
+        </div>
+      ))}
+    </>
   );
 }
