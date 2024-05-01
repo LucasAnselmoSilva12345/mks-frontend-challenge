e14f3d645eaef9bc62ace2179114ba236a80af49
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-05-01 16:12:33 -0300

    feat: Footer component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import '../scss/footer.scss';
+
+export function Footer() {
+  return (
+    <footer className="footer-container">
+      <p className="footer-container-wrapper-title">
+        MKS sistemas © Todos os direitos reservados
+      </p>
+    </footer>
+  );
+}
